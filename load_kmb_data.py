5f30a016ce88704f79cc3f1375828584f13578b0
Karl <karl@ltek.me>
2024-10-03 10:05:04 +0800

    cleanup failed

--- load_kmb_data.py
+++ load_kmb_data.py
@@ -1,26 +1,32 @@
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_chroma import Chroma
+from google.oauth2.service_account import Credentials
+from langchain_text_splitters import RecursiveJsonSplitter
 import json
+
+
 kmb_loaded = json.load(open('./bus.json', encoding='utf-8-sig'))
 kmb_loaded = kmb_loaded['features']
+
+stops = [f"coordinates " for s in kmb_loaded]
 print("Total number of bus stops: ", len(kmb_loaded))
 
-from langchain_text_splitters import RecursiveJsonSplitter
 
 splitter = RecursiveJsonSplitter()
 splits = splitter.create_documents(kmb_loaded)
 # print(splits[0:5])
 print("Total number of splits: ", len(splits))
 
+
 def divide_chunks(l, n):
     # looping till length l
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
+
 chunks = list(divide_chunks(splits, 1000))
 print("Total number of chunks: ", len(chunks))
 
-from google.oauth2.service_account import Credentials
-from langchain_chroma import Chroma
-from langchain_google_vertexai import VertexAIEmbeddings
 
 credentials = Credentials.from_service_account_file(
     "ive-fyp-436703-3a208c6d96a0.json")
