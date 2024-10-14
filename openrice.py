6546a09403997410536a23f5cafedc791a5b33d8
Karl <karl@ltek.me>
2024-10-14 21:06:25 +0800

    add caching data and fix data fetching

--- openrice.py
+++ openrice.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import shutil
 from typing import Tuple
 
 
@@ -31,7 +32,7 @@ class OpenriceApi(object):
         "Referrer-Policy": "strict-origin-when-cross-origin"
     }
 
-    lang_dict_options = ["tc", "en", "sc"]
+    lang_dict_options: dict = ["tc", "en", "sc"]
 
     base_data_path = "./openrice_data/"
     raw_price_range_data_path: str = base_data_path + "openrice_priceRange_raw.json"
@@ -40,15 +41,20 @@ class OpenriceApi(object):
     district_data_path: str = base_data_path + "openrice_district.json"
 
     store_data: bool = True
+    force_fetch: bool = False
 
-    def write_data(self, data, path, force=False):
-        if os.path.exists(path) and not force:
-            print(f"Data already exists at {path}. Skipping write.")
-            return
+    raw_district_data: dict = {}
+    raw_price_range_data: dict = {}
+    district_data: list = []
+    price_range_data: list = []
+
+    def write_data(self, data: dict | list, path: str) -> None:
+        print("Writing data to:", path)
         with open(path, "w") as f:
             json.dump(data, f, indent=4)
 
-    def fetch(self, url, params: dict = {}):
+    def fetch(self, url: str, params: dict = {}) -> dict | list:
+        print("Fetching data from:", url)
         response = requests.request(
             method=params.get("method", "GET"),
             url=url,
@@ -59,82 +65,121 @@ class OpenriceApi(object):
 
     def fetch_price_range_data(self) -> Tuple[list, list]:
         # Return (raw, processed) pricerange data
-        raw_price_range_data = self.fetch(self.PRICE_RANGE_DATA_URL)
+        # Caching cluster fuck
+        if self.price_range_data != [] and self.raw_price_range_data != {}:
+            return (self.raw_price_range_data, self.price_range_data)
+
+        raw_price_range_data = {}
+        if self.raw_price_range_data != {}:
+            raw_price_range_data = self.raw_price_range_data
+        elif self.force_fetch:
+            raw_price_range_data = self.fetch(self.PRICE_RANGE_DATA_URL)
+        elif self.store_data and os.path.exists(self.raw_price_range_data_path):
+            with open(self.raw_price_range_data_path, "r") as f:
+                raw_price_range_data = json.load(f)
+        else:
+            raw_price_range_data = self.fetch(self.PRICE_RANGE_DATA_URL)
+        self.raw_price_range_data = raw_price_range_data
+
+        # Doc formatting
         price_range_data = [{
-            "nameId": data["nameLangDict"]["en"],
-            "rangeId": f"from-{data['rangeStart']}-to-{data['rangeEnd']}",
-            "nameLangDict": {
-                "tc": data['nameLangDict']['tc'],
-                "en": data['nameLangDict']['en'],
-                "sc": data['nameLangDict']['sc'],
-            },
+            "rangeId": data["priceRangeId"],
+            "nameId": f"from-{data['rangeStart']}-to-{data['rangeEnd']}",
+            "nameLangDict": {key: data['nameLangDict'][key] for key in self.lang_dict_options},
         } for data in raw_price_range_data["regions"]['0']['priceRanges']]
+
+        # Caching cluster fuck
+        self.price_range_data = price_range_data
         return (raw_price_range_data, price_range_data)
 
     def fetch_district_data(self) -> Tuple[list, list]:
         # Return (raw, processed) district data
-        raw_district_data = self.fetch(self.DISTRICT_DATA_URL)
+        # Caching cluster fuck
+        if self.district_data != [] and self.raw_district_data != {}:
+            return (self.raw_district_data, self.district_data)
+
+        raw_district_data = {}
+        if self.raw_district_data != {}:
+            raw_district_data = self.raw_district_data
+        elif self.force_fetch:
+            raw_district_data = self.fetch(self.DISTRICT_DATA_URL)
+        elif self.store_data and os.path.exists(self.raw_district_data_path):
+            with open(self.raw_district_data_path, "r") as f:
+                raw_district_data = json.load(f)
+        else:
+            raw_district_data = self.fetch(self.DISTRICT_DATA_URL)
+        self.raw_district_data = raw_district_data
+
+        # Doc formatting
         district_data = [{
-            "nameId": data["nameLangDict"]["en"],
+            "nameId": data["callNameLangDict"]["en"],
             "districtId": data["districtId"],
-            "nameLangDict": {
-                "tc": data['nameLangDict']['tc'],
-                "en": data['nameLangDict']['en'],
-                "sc": data['nameLangDict']['sc'],
-            },
+            "nameLangDict": {key: data['nameLangDict'][key] for key in self.lang_dict_options},
         } for data in raw_district_data["districts"]]
+
+        # Caching cluster fuck
+        self.district_data = district_data
+
         return (raw_district_data, district_data)
 
-    def get_price_range_data(self, force=False):
+    def get_price_range_data(self) -> list:
         if not self.store_data:
             return self.fetch_price_range_data()[1]
-        if not os.path.exists(self.price_range_data_path) or force:
+        if not os.path.exists(self.price_range_data_path) or self.force_fetch:
             raw, processed = self.fetch_price_range_data()
             if self.store_data:
-                self.write_data(raw, self.raw_price_range_data_path)
-                self.write_data(processed, self.price_range_data_path)
+                if not os.path.exists(self.raw_price_range_data_path):
+                    create_folder_if_not_exists(self.base_data_path)
+                    self.write_data(raw, self.raw_price_range_data_path)
+                if not os.path.exists(self.price_range_data_path):
+                    create_folder_if_not_exists(self.base_data_path)
+                    self.write_data(processed, self.price_range_data_path)
             return processed
         with open(self.price_range_data_path, "r") as f:
             return json.load(f)
 
-    def get_district_data(self, force=False):
+    def get_district_data(self) -> list:
         if not self.store_data:
             return self.fetch_district_data()[1]
-        if not os.path.exists(self.district_data_path) or force:
+        if not os.path.exists(self.district_data_path) or self.force_fetch:
             raw, processed = self.fetch_district_data()
             if self.store_data:
-                self.write_data(raw, self.raw_district_data_path)
-                self.write_data(processed, self.district_data_path)
+                if not os.path.exists(self.raw_district_data_path):
+                    create_folder_if_not_exists(self.base_data_path)
+                    self.write_data(raw, self.raw_district_data_path)
+                if not os.path.exists(self.district_data_path):
+                    create_folder_if_not_exists(self.base_data_path)
+                    self.write_data(processed, self.district_data_path)
             return processed
         with open(self.district_data_path, "r") as f:
             return json.load(f)
 
-    def convert_loc_to_google_map_url(self, lat, lng):
+    def convert_loc_to_google_map_url(self, lat: float, lng: float) -> str:
         # https://www.google.com/maps/place/22.2814411,114.1564406
         return f"https://www.google.com/maps/place/{lat},{lng}"
 
-    def get_price_range_text_from_id(self, price_range_id, lang="tc"):
+    def get_price_range_text_from_id(self, price_range_id: int, lang: str = "tc") -> str | None:
         price_range_data = self.get_price_range_data()
         for price_range in price_range_data:
             if price_range["rangeId"] == price_range_id:
                 return price_range["nameLangDict"][lang]
         return None
 
-    def get_district_text_from_id(self, district_id, lang="tc"):
+    def get_district_text_from_id(self, district_id: int, lang: str = "tc") -> str | None:
         district_data = self.get_district_data()
         for district in district_data:
             if district["districtId"] == district_id:
                 return district["nameLangDict"][lang]
         return None
 
-    def get_district_id_from_text(self, district_text, lang="tc"):
+    def get_district_id_from_text(self, district_text: int, lang: str = "tc") -> str | None:
         district_data = self.get_district_data()
         for district in district_data:
             if district["nameLangDict"][lang] == district_text:
                 return district["districtId"]
         return None
 
-    def search_restaurants(self, params):
+    def search_restaurants(self, params) -> list | None:
         # params: {
         #     "keyword": restaurant keyword,
         #     "start": strating pagination index -> 0,
@@ -193,21 +238,16 @@ class OpenriceApi(object):
 
     def __init__(self,
                  base_data_path="./openrice_data",
-                 fetch_data=True,
+                 force_fetch=False,
                  store_data=True,
                  ):
         self.store_data = store_data
+        self.force_fetch = force_fetch
         self.data_base_dir = base_data_path
-        if fetch_data and store_data:
+        if store_data:
+            if force_fetch and os.path.exists(self.data_base_dir):
+                shutil.rmtree(self.data_base_dir)
             create_folder_if_not_exists(self.data_base_dir)
-            if not os.path.exists(self.price_range_data_path):
-                raw, processed = self.fetch_price_range_data()
-                self.write_data(raw, self.raw_price_range_data_path)
-                self.write_data(processed, self.price_range_data_path)
-            if not os.path.exists(self.district_data_path):
-                raw, processed = self.fetch_district_data()
-                self.write_data(raw, self.raw_district_data_path)
-                self.write_data(processed, self.district_data_path)
 
     def __repr__(self) -> str:
         return f"OpenriceApi(base_data_path={self.base_data_path}, store_data={self.store_data})"
