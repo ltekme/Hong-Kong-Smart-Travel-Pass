1a4c2025d2f1e05c43b0c9b5ba6ac034511b0e03
Karl <karl@ltek.me>
2024-10-20 14:22:42 +0800

    add system message support

--- mock_chat_llm.py
+++ mock_chat_llm.py
@@ -13,6 +13,28 @@ from langchain.agents import tool
 from google.oauth2.service_account import Credentials
 
 
+class LLMChainModel:
+    def __init__(self,
+                 credentials: Credentials,
+                 model: str,
+                 temperature: float,
+                 max_tokens: int,
+                 ):
+        self.llm = ChatVertexAI(
+            model=model,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            timeout=None,
+            max_retries=2,
+            credentials=credentials,
+            project=credentials.project_id,
+            region="us-central1",
+        )
+
+    def invoke(self, messages: list):
+        return self.llm.invoke(messages)
+
+
 class ChatMessage:
 
     def __init__(self, role: str, content: str, **kwargs) -> None:
@@ -20,12 +42,15 @@ class ChatMessage:
         self.message = content
 
     def prompt_message(self):
-        if self.role == "ai":
-            return AIMessage(self.message)
-        elif self.role == "user":
-            return HumanMessage(self.message)
-        else:
-            raise ValueError("role must be 'ai' or 'user'")
+        roles = {
+            "user": HumanMessage,
+            "ai": AIMessage,
+            "system": SystemMessage,
+        }
+        message = roles.get(self.role, None)
+        if not message:
+            raise ValueError(f"role must be one of {roles.keys()}")
+        return message(self.message)
 
     def to_dict(self):
         return {
@@ -38,10 +63,17 @@ class ChatMessages:
 
     _chat_messages: t.List[ChatMessage] = []
 
+    def __init__(self, system_message_string: str | None = None) -> None:
+        if system_message_string:
+            self._chat_messages.append(
+                ChatMessage('system', system_message_string))
+
     def get_all_as_dict(self) -> list:
         return [chatMessage.to_dict() for chatMessage in self._chat_messages]
 
     def append(self, chatMessage: ChatMessage) -> None:
+        if chatMessage.role not in ['user', 'ai']:
+            raise ValueError("role must be one of ['user', 'ai']")
         self._chat_messages.append(chatMessage)
 
     def save_to_file(self, file_path: str) -> None:
@@ -52,42 +84,64 @@ class ChatMessages:
                       for chatMessage in self._chat_messages], f, indent=4)
 
     def get_from_file(self, file_path: str) -> list:
+        system_message = None
+        if len(self._chat_messages) > 0 and self._chat_messages[0].role == 'system':
+            system_message = self._chat_messages[0]
         if not os.path.exists(file_path) or not os.path.isfile(file_path):
             self._chat_messages = []
+            if system_message:
+                self._chat_messages.append(system_message)
             return self._chat_messages
         try:
-            data_in_file = json.load(open(file_path, 'r'))
+            with open(file_path, 'r') as f:
+                data_in_file = json.load(f)
             self._chat_messages = [ChatMessage(**msg) for msg in data_in_file]
+            if len(self._chat_messages) > 0 and system_message:
+                if self._chat_messages[0].role != 'system':
+                    self._chat_messages.insert(0, system_message)
+                if self._chat_messages[0].role == 'system' and self._chat_messages[0].message != system_message.message:
+                    self._chat_messages[0].message = system_message.message
             return self._chat_messages
         except json.JSONDecodeError:
             self._chat_messages = []
+            if system_message:
+                self._chat_messages.append(system_message)
             return self._chat_messages
+
+    def add_system_prompt(self, message: str) -> ChatMessage:
+        if len(self._chat_messages) > 0 and self._chat_messages[0].role == 'system':
+            self._chat_messages[0].message = message
+            return self._chat_messages[0]
+        system_message = ChatMessage('system', message)
+        self._chat_messages.insert(0, system_message)
+        return system_message
 
 
 class ChatLLM:
 
-    chatRecords = ChatMessages()
+    chatRecords = ChatMessages(
+        system_message_string="""You are a very powerful AI. 
+        Context maybe given to assist the assistant in providing better responses.
+        Answer the questions to the best of your ability.
+        If you don't know the answer, just say you don't know."""
+    )
     chatRecordFolderPath = './chat_data'
     store_chat_records = True
     _chatId = str(uuid.uuid4())
 
     def __init__(self,
                  credentials: Credentials,
-                 model: str = "gemini-1.0-pro-vision",
+                 model: str = "gemini-1.5-pro",
                  temperature: float = 0.9,
-                 max_tokens: int = 2048,
+                 max_tokens: int = 4096,
                  chatRecordFolderPath: str = './chat_data',
                  chatId: str = None,
                  store_chat_records: bool = True):
-        self.llm = ChatVertexAI(
+        self.llm = LLMChainModel(
+            credentials=credentials,
             model=model,
             temperature=temperature,
-            max_tokens=max_tokens,
-            timeout=None,
-            max_retries=2,
-            credentials=credentials,
-            project=credentials.project_id,
-            region="us-central1",
+            max_tokens=max_tokens
         )
         self._chatId = chatId or self._chatId
         self.chatRecordFolderPath = chatRecordFolderPath
@@ -115,6 +169,8 @@ class ChatLLM:
         if not message:
             return ChatMessage('', "Please provide a message.")
         self.chatRecords.append(ChatMessage('user', message))
+
+        # print(self.chatRecords.get_all_as_dict())
 
         resault = self.llm.invoke(self.chatRecords.get_all_as_dict())
 
